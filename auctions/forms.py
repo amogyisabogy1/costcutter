d797956de5b2eff6c2eac6e007a4b1fb166588df
Amogh Ganjikunta <amoghganjikunta@gmail.com>
2022-06-05 00:00:09 -0400

    commit message

---
+++ auctions/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Category, Auction, Person
+
+
+class AuctionForm(forms.ModelForm):
+
+    class Meta:
+        model = Auction
+        fields = ('title', 'description', 'Costs',
+                  'category', 'person', 'receipt')
+        widgets = {'category': forms.Select(choices=Category.objects.all(), attrs={'class': 'form-control'}),
+                   'person': forms.Select(choices=Person.objects.all(), attrs={'class': 'form-control'}),
+                   'title': forms.TextInput(attrs={'class': 'form-control'}),
+                   'description': forms.TextInput(attrs={'class': 'form-control'}),
+                   'Costs': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Cost', 'title': 'Costs'})}
